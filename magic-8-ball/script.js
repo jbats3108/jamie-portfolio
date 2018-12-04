be5b6799870ddc5fe710495ad94c278aa382acc0
jbats3108 <jamiebatabyal@gmail.com>
2018-12-04 12:29:14 +0800

    tidy code according to JSLint

--- magic-8-ball/script.js
+++ magic-8-ball/script.js
@@ -1,55 +1,67 @@
-$(document).ready(function(){
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
 
-var magic8Ball = {
+$(document).ready(function () {
 
-	//Possible answers to be displayed
-	listOfAnswers: ["Yes", "No", "Maybe", "I don't know", "Can you repeat the question?"],
+    "use strict";
+    var magic8Ball = {
 
-	//Method to run when question is asked
-	askAQuestion: function ask(question) {
+            //Possible answers to be displayed
+            listOfAnswers: ["Yes",
+                            "No",
+                            "Maybe",
+                            "I don't know",
+                            "Can you repeat the question?"],
 
-	//Shake the 8 ball
-	$("#8ball").effect("shake");
+            //Method to run when question is asked
+            askAQuestion: function askAQuestion() {
 
+                //Shake the 8 ball
+                $("#8ball").effect("shake");
 
-	//Switch image to answer side after 1s
-	setTimeout(function(){
-	$("#8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
-	}, 1000);
+                //Switch image to answer side after 1s
+                setTimeout(function () {
+                    $("#8ball").attr("src",
+                        "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+                }, 1000);
 
-	//Randomly select nr between 0 and length -1 to use as index in order to select a random answer
-	var answer = this.listOfAnswers[Math.floor(Math.random()*this.listOfAnswers.length)];
+                //Randomly select index in order to select a random answer
+                var answer = this.listOfAnswers[Math.floor(Math.random()
+                    * this.listOfAnswers.length)];
 
-	//Set answer text to the selected value
-	$("#answer").text(answer);
+                //Set answer text to the selected value
+                $("#answer").text(answer);
 
-	//Fade answer in over 4s, 1s after answer side appears
-	setTimeout(function(){
-	$("#answer").fadeIn(4000);},1000);
-	}
-};
+                //Fade answer in over 4s, 1s after answer side appears
+                setTimeout(function () {
+                    $("#answer").fadeIn(4000);
+                }, 1000);
+            }
+        };
 
+    var onClick = function () {
+        //Hide previous answer
+        $("#answer").hide();
 
-var onClick = function(){
-	//Hide previous answer
-	$("#answer").hide();
+        //Switch back to 8 side
+        $("#8ball").attr("src",
+            "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/8side.png");
 
-	//Switch back to 8 side
-	$("#8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/8side.png");
+        //Wait 500ms before showing prompt
+        setTimeout(
+            function () {
+                //Box to ask the question
+                var question = this.prompt("ASK A YES/NO QUESTION!");
 
-	//Wait 500ms before showing prompt
-	setTimeout(
-		function(){
-			//Box to ask the question
-			var question = prompt("ASK A YES/NO QUESTION!");
+                //Call method to display answer
+                magic8Ball.askAQuestion(question);
+            },
+            500
+        );
 
-			//Call method to display answer
-			magic8Ball.askAQuestion(question);
-			}, 500);
+    };
 
-};
-
-//Call the onClick function when question button is clicked
-$("#questionButton").click(onClick);
+    //Call the onClick function when question button is clicked
+    $("#questionButton").click(onClick);
 
 });
