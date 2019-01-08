8e8c34e557b2ba525666009060557156be6920e4
jbats3108 <jamiebatabyal@gmail.com>
2019-01-08 16:23:12 +0800

    add js to expand/collapse project details

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+/*jslint browser:true */
+
+/*global
+alert, confirm, console, prompt
+*/
+
 /* JS */
+
+var i,
+    header = document.getElementsByClassName("reveal");
+
+
+var toggle = function () {
+    "use strict";
+    var projectDescription = this.parentElement.children[2],
+        pdd = projectDescription.style.display;
+
+    if (pdd === "block") {
+        projectDescription.style.display = "none";
+
+    } else {
+        projectDescription.style.display = "block";
+    }
+};
+
+
+for (i = 0; i < header.length; i += 1) {
+    header[i].addEventListener("click", toggle);
+}
