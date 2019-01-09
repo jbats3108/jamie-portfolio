28b4731b174e5942dd2daf3491f349710eb56c4d
jbats3108 <jamiebatabyal@gmail.com>
2019-01-09 12:47:45 +0800

    Added Text Change & Resizing of Projects section to js

--- script.js
+++ script.js
@@ -3,27 +3,40 @@
 /*global
 alert, confirm, console, prompt
 */
-
-/* JS */
-
 var i,
-    header = document.getElementsByClassName("reveal");
+    header = document.getElementsByClassName("reveal"),
+    pSection = document.getElementById("projects");
 
 
-var toggle = function () {
+var toggleDesc = function () {
     "use strict";
-    var projectDescription = this.parentElement.children[2],
-        pdd = projectDescription.style.display;
+    var pDesc = this.parentElement.children[2];
 
-    if (pdd === "block") {
-        projectDescription.style.display = "none";
-
+    //Change Text & Show / Hide Content
+    if (this.innerHTML === '<button type="button" onclick="click">Hide</button>') {
+        this.innerHTML = '<button type="button" onclick="click">Details...</button>';
+        pDesc.style.display = "none";
     } else {
-        projectDescription.style.display = "block";
+        this.innerHTML = '<button type="button" onclick="click">Hide</button>';
+        pDesc.style.display = "block";
+    }
+
+    var pDesc0 = pSection.children[2].children[2],
+        pDesc1 = pSection.children[3].children[2],
+        pDesc2 = pSection.children[4].children[2],
+        pdd0 = pDesc0.style.display,
+        pdd1 = pDesc1.style.display,
+        pdd2 = pDesc2.style.display;
+
+    // Resize section if no content showing
+    if (pdd0 !== "block" &&  pdd1 !== "block" && pdd2 !== "block") {
+        pSection.style.height = "200px";
+    } else {
+        pSection.style.height = "300px";
     }
 };
 
-
+// Add Event Listener to Click on Details... buttons
 for (i = 0; i < header.length; i += 1) {
-    header[i].addEventListener("click", toggle);
+    header[i].addEventListener("click", toggleDesc);
 }
