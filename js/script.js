c80432a42de8e944b4dc953077699cabdc48d018
jbats3108 <jamiebatabyal@gmail.com>
2019-01-18 16:04:04 +0800

    change wording for details to details [+], beautify code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-$(document).ready(function(){
-
-  $('.reveal').on('click', function(){
-
-    $(this).parent().children().last().toggle();
+$(document).ready(function() {
+  $(".reveal").on("click", function() {
+    $(this)
+      .parent()
+      .children()
+      .last()
+      .toggle();
     // console.log($(this).text());
-    $(this).children().html(function(i, text){
-        return text === "Details..." ? "Hide" : "Details...";
-    });
+    $(this)
+      .children()
+      .html(function(i, text) {
+        return text === "Details [+]" ? "Hide [-]" : "Details [+]";
+      });
   });
 });
